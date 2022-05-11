7ddaf72e3fb5a03704ef2b61eb37929fcc83f273
damar <damarisriverosg@gmail.com>
2022-05-11 18:29:08 -0400

    agregado modelo

--- musica/models.py
+++ musica/models.py
@@ -11,9 +11,9 @@ class TipoUsuario(models.Model):
 class Usuario(models.Model):
     idusuario = models.IntegerField(primary_key=True,verbose_name='Id')
     nombreUsuario = models.CharField(max_length=50,verbose_name='nombre')
-    Contrasenna = models.CharField(max_length=6,primary_key=True, verbose_name='contrasenna')
-    Fecha_nac = models.CharField(max_length=20,null=True, blank=True, verbose_name='Modelo')
+    Contrasenna = models.CharField(max_length=6, verbose_name='contrasenna')
+    Fecha_nac = models.DateField()
     usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.categoria
+        return self.nombreUsuario
