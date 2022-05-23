fd21ac83eab7d6175c7e429e3ce41d6c908e353a
damar <damarisriverosg@gmail.com>
2022-05-23 15:56:02 -0400

    agregar modelos

--- musica/models.py
+++ musica/models.py
@@ -17,3 +17,19 @@ class Usuario(models.Model):
 
     def __str__(self):
         return self.nombreUsuario
+class TipoEvento(models.Model):
+    idEvento = models.IntegerField(primary_key=True,verbose_name='Id de Evento')
+    nombreTipoEvento = models.CharField(max_length=60,verbose_name='Nombre Tipo de Evento')
+
+    def __str__(self):
+        return self.nombreTipoEvento
+
+class Evento(models.Model):
+    nombreEvento = models.CharField(max_length=60,primary_key=True, verbose_name='Nombre Evento')
+    descripcion = models.CharField(max_length=300, verbose_name='Descripción')
+    fecha = models.DateField()
+    precio = models.IntegerField(null=True, verbose_name='Precio')
+    categoria = models.ForeignKey(TipoEvento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombreEvento
