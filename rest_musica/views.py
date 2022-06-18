e2e2c6808305c7951364e4eac53ecf57a60397ff
damar <damarisriverosg@gmail.com>
2022-06-18 14:45:05 -0400

    arreglo de fecha

--- rest_musica/views.py
+++ rest_musica/views.py
@@ -1,35 +1,44 @@
 from wsgiref.util import request_uri
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from musica.models import Evento
 from rest_musica.serializers import eventoSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 @csrf_exempt
 @api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
 def lista_evento(request):
     if request.method =='GET':
         listaEvento = Evento.objects.all()
         serializer = eventoSerializer(listaEvento, many= True)
         return Response(serializer.data)
-    elif request == 'POST':
+    elif request.method == 'POST':
         dataP = JSONParser().parse(request)
+        print(dataP)
         serializer =eventoSerializer(data=dataP)
+        print(serializer)
         if serializer.is_valid():
+            print("paso1")
             serializer.save()
+            print("paso2")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
+            print("paso3")
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET','PUT','DELETE'])
-def detalle_evento(request, id):
+@permission_classes((IsAuthenticated,))
+def detalle_evento(request, eve):
     try:
-        evento= Evento.objects.get(nombreEvento=id)
+        evento= Evento.objects.get(nombreEvento=eve)
     except Evento.DoesNotExist:
         return Response(status= status.HTTP_404_NOT_FOUND)
     if request.method =='GET':
