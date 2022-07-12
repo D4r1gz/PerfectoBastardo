9000bb96878645c76472c8f58220498eb57d32cf
damar <damarisriverosg@gmail.com>
2022-07-11 22:03:47 -0400

    asd

--- perfectoBastardo/urls.py
+++ perfectoBastardo/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-=======
 from django.conf import settings
 from django.conf.urls.static import static
->>>>>>> damaris2
 
 
 urlpatterns = [
